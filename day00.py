6ddc2765b25b6aef870815079c1d558c222c560a
John Donnellan <john@codybartfast.com>
2024-12-04 08:15:16 +0000

    ✖️

--- day00.py
+++ day00.py
@@ -1,17 +1,19 @@
+INPUT = 'test1'
 
-def text():
-    return open(f'./input/2024/day00/{input_name}.txt').read().strip()
+def main():
+    items = [parse(line) for line in lines()]
 
-def lines(): return text().splitlines()
+    ans1 = '\n'.join([str(item) for item in items])
+    ans2 = ', '.join(open(f'./input/2024/day00/input.txt', 'r').read().splitlines())
+
+    print(f'Part 1: {ans1}\nPart 2: {ans2}')
 
 def parse(line):
     return line
 
-input_name = 'test1'
-items = [parse(line) for line in lines()]
+def lines(): return read().splitlines()
 
-ans1 = '\n'.join([str(item) for item in items])
-ans2 = ', '.join(open(f'./input/2024/day00/input.txt', 'r').read().splitlines())
+def read():
+    return open(f'./input/2024/day00/{INPUT}.txt').read().strip()
 
-print(f'Part 1: {ans1}')
-print(f'Part 2: {ans2}')
+main()
---
+++ day04.py
@@ -0,0 +1,51 @@
+INPUT = 'input'
+DIRECTIONS = ((0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1))
+
+
+def main():
+    puzzle = lines()
+    xmas_count, x_mas_count = 0, 0
+    for y in range(len(puzzle)):
+        for x in range(len(puzzle[y])):
+            for direction in DIRECTIONS:
+                if check_xmas(puzzle, 'XMAS', (x, y), direction):
+                    xmas_count += 1
+            if check_x_mas(puzzle, (x, y)):
+                x_mas_count += 1
+    print(f'Part 1: {xmas_count}\nPart 2: {x_mas_count}')
+
+
+def check_x_mas(puzzle, pos):
+    points = ''.join(
+        letter_at(puzzle, take_step(pos, direction))
+        for direction in DIRECTIONS[1::2])
+
+    return letter_at(puzzle, pos) == 'A' and points in ['MMSS', 'SMMS', 'SSMM', 'MSSM']
+
+
+def check_xmas(puzzle, remaining, pos, direction):
+    if not remaining:
+        return True
+    if not letter_at(puzzle, pos) == remaining[0]:
+        return False
+    return check_xmas(puzzle, remaining[1:], take_step(pos, direction), direction)
+
+
+def take_step(pos, step):
+    col, row = pos
+    col_delta, row_delta = step
+    return col + col_delta, row + row_delta
+
+
+def letter_at(puzzle, pos):
+    col, row = pos
+    if row < 0 or row >= len(puzzle) or col < 0 or col >= len(puzzle[0]):
+        return '#'
+    return puzzle[row][col]
+
+
+def lines():
+    return open(f'./input/2024/day04/{INPUT}.txt').read().strip().splitlines()
+
+
+main()
