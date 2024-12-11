f2fa1eba9e4404235d2e2ddeefd21f4821a2ced6
John Donnellan <john@codybartfast.com>
2024-12-11 13:16:07 +0000

    🌉

--- day07.py
+++ day07.py
@@ -1,22 +1,21 @@
 INPUT = 'input'
 
+
 def main():
-    equations = [parse(line) for line in lines()]
+    equations = [parse(line) for line in read().splitlines()]
 
-
-    ans1 = sum([
+    ans1 = sum(
         result
         for result, operands in equations
-        if any(result == value for value in variations(operands))
-    ])
+        if any(result == value for value in variations(operands, False)))
 
-    ans2 = sum([
+    ans2 = sum(
         result
         for result, operands in equations
-        if any(result == value for value in variations(operands, True))
-    ])
+        if any(result == value for value in variations(operands, True)))
 
     print(f'Part 1: {ans1}\nPart 2: {ans2}')
+
 
 def concatenate(a, b):
     factor = 10
@@ -24,27 +23,28 @@ def concatenate(a, b):
         factor *= 10
     return a * factor + b
 
-def variations(operands, include_concatenate = False):
+
+def variations(operands, include_concatenate=False):
     [*rest, operand] = operands
-    # print(operand)
     if not rest:
         yield operand
     else:
         yield from (
             result
             for variation in variations(rest, include_concatenate)
-            for result in ([variation + operand, variation * operand, concatenate(variation, operand)]
+            for result in ((variation + operand, variation * operand, concatenate(variation, operand))
                            if include_concatenate
-                           else [variation + operand, variation * operand])
+                           else (variation + operand, variation * operand))
         )
 
+
 def parse(line):
-    [test_value, *operands] = [int(part.strip(':')) for part in line.split(' ')]
+    (test_value, *operands) = [int(part.strip(':')) for part in line.split(' ')]
     return test_value, operands
 
-def lines(): return read().splitlines()
 
 def read():
     return open(f'./input/2024/day07/{INPUT}.txt').read().strip()
+
 
 main()
