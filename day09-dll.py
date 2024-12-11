92418f63981dcd5364830f0295a742e51c95a89e
John Donnellan <john@codybartfast.com>
2024-12-11 21:09:35 +0000

    💿

--- day09-dll.py
+++ day09-dll.py
@@ -1,27 +1,62 @@
 INPUT = 'input'
 
-class File:
-    def __init__(self, id, size, space):
-        self.id = id
-        self.size = size
-        self.space = space
-        self.next = None
-        self.prev = None
-
-    def __str__(self):
-        return f'{self.prev.id if self.prev else self.prev}<-F{self.id}:{self.size}..{self.space}->{self.next.id if self.next else self.next}'
-
 
 def main():
-    map = read()
-    map = [int(digit) for digit in map]
-    if len(map) % 2 == 0:
-        raise ValueError('Exepected odd map length')
-    map.append(0)
+    disk_map = [int(digit) for digit in read()]
+    disk_map.append(0)
 
-    disk = init_disk(map)
+    ans1 = checksum(defrag_blocks(disk_map))
+    print(f'Part 1: {ans1}')
+    ans2 = checksum(defrag_files(disk_map))
+    print(f'Part 2: {ans2}')
+
+
+def defrag_files(disk_map):
+    files = [File(idx // 2, disk_map[idx - 1], disk_map[idx])
+             for idx in range(1, len(disk_map), 2)]
+    file_list = DoublyLinkedList(files)
+    source_item = file_list.tail
+
+    while source_item:
+        if target_item := find_space(file_list, source_item):
+            source = source_item.value
+            target = target_item.value
+            source_prev_item = file_list.remove(source_item)
+            source_prev = source_prev_item.value
+
+            source_prev.space += source.size + source.space
+            source.space = target.space - source.size
+            target.space = 0
+            file_list.insert_after(source, target_item)
+            source_item = source_prev_item
+        else:
+            source_item = source_item.prev
+    return expand_files(file_list)
+
+
+def expand_files(disk):
+    blocks = []
+    for item in disk:
+        file = item.value
+        blocks += [file.id] * file.size
+        blocks += [None] * file.space
+        item = item.next
+    return blocks
+
+
+def find_space(disk, item_to_move):
+    item = disk.head
+    while item and item != item_to_move:
+        if item.value.space >= item_to_move.value.size:
+            return item
+        item = item.next
+
+
+def defrag_blocks(disk_map):
+    disk = expand_map(disk_map)
     fill = 0
     move = len(disk) - 1
+
     def search():
         nonlocal fill
         nonlocal move
@@ -29,117 +64,106 @@ def main():
             fill += 1
         while disk[move] == None:
             move -= 1
+
     search()
     while fill < move:
         disk[fill] = disk[move]
         disk[move] = None
         search()
-    ans1 = checksum(disk)
-
-    files = init_files(map)
-    for id in range(len(files) - 1, -1, -1):
-        try_move_file(files, id)
-
-    # try_move_file(files, 9)
-    # try_move_file(files, 8)
-    # try_move_file(files, 7)
-    # try_move_file(files, 6)
-    # try_move_file(files, 5)
-    # try_move_file(files, 4)
-    # try_move_file(files, 3)
-    # try_move_file(files, 2)
-    # try_move_file(files, 1)
-    # try_move_file(files, 0)
-
-    file = files[0]
-    while file:
-        print(file)
-        file = file.next
-
-    print(checksum_file(files))
-
-    disk2 = []
-    file = files[0]
-    while file:
-        disk2 += [file.id] * file.size
-        disk2 += [None] * file.space
-        file = file.next
-
-    ans2 = checksum(disk2)
-    print(f'Part 1: {ans1}')
-    print(f'Part 2: {ans2}')
-
-def checksum_file(files):
-    sum = 0
-    pos = 0
-    file = files[0]
-    while file:
-        for block in range(0, file.size):
-            sum += file.id * pos
-            pos += 1
-        pos = pos + file.space
-        file = file.next
-    return sum
-
-def try_move_file(files, id):
-    # print(id)
-    source = files[id]
-    destination = files[0]
-    while destination.id != id:
-        if destination.space >= source.size:
-            if destination.next == source:
-                destination.space = 0
-                source.space += destination.space
-                return True
-            source_prev = source.prev
-            source_next = source.next
-            source_space = source.space
-            source.space = destination.space - source.size
-            destination.space = 0
-            source.next = destination.next
-            destination.next.prev = source
-            source.prev = destination
-            destination.next = source
-
-            source_prev.space += source.size + source_space
-            source_prev.next = source_next
-            if source_next:
-                source_next.prev = source_prev
-            return True
-        destination = destination.next
-    return False
-
-
-def init_files(map):
-    files = {}
-    for idx in range(0, len(map), 2):
-        file = File(idx // 2, map[idx], map[idx+1])
-        files[file.id] = file
-    for id in range(0, len(files)-1):
-        files[id].next = files[id+1]
-        files[id+1].prev = files[id]
-    return files
+    return disk
 
 
 def checksum(disk):
     return sum(idx * id for (idx, id) in enumerate(disk) if id != None)
 
-def parse(line):
-    return line
 
-def init_disk(map):
+def expand_map(disk_map):
     disk = []
     id = 0
-    for idx in range(0, len(map), 2):
-        for i in range(map[idx]):
-            disk.append(id)
-        for i in range(map[idx+1]):
-            disk.append(None)
+    for idx in range(0, len(disk_map), 2):
+        disk += [id] * disk_map[idx]
+        disk += [None] * disk_map[idx + 1]
         id += 1
     return disk
 
 
 def read():
     return open(f'./input/2024/day09/{INPUT}.txt').read().strip()
+
+
+class File:
+    def __init__(self, id, size, space):
+        self.id = id
+        self.size = size
+        self.space = space
+
+    def __repr__(self):
+        return f'{self.id}x{self.size}[.{self.space}.]'
+
+
+class DoublyLinkedList:
+    class Item:
+        def __init__(self, value, prev, next):
+            self.value = value
+            self.prev = prev
+            self.next = next
+
+        def __repr__(self):
+            prev_id = self.prev.value.id if self.prev else None
+            next_id = self.next.value.id if self.next else None
+            return f'{prev_id}<-{self.value}->{next_id}'
+
+    def __init__(self, values=[]):
+        self.head = None
+        self.tail = None
+        self.len = 0
+        for value in values:
+            self.insert_after(value)
+
+    def insert_after(self, value, prev=None):
+        if not self.head:
+            self.head = self.Item(value, None, None)
+            self.tail = self.head
+            self.len += 1
+            return self.head
+        if not prev:
+            prev = self.tail
+        next = prev.next
+        item = DoublyLinkedList.Item(value, prev, next)
+        prev.next = item
+        if next:
+            next.prev = item
+        if prev == self.tail:
+            self.tail = item
+        self.len += 1
+        return item
+
+    def remove(self, item):
+        prev = item.prev
+        next = item.next
+        if prev:
+            prev.next = next
+        if next:
+            next.prev = prev
+        if item == self.head:
+            self.head = next
+        if item == self.tail:
+            self.tail = prev
+        self.len -= 1
+        return prev
+
+    def __iter__(self):
+        item = self.head
+        while item:
+            yield item
+            item = item.next
+
+    def __len__(self):
+        return self.len
+
+    def __repr__(self):
+        item_strings = [str(item) for item in self]
+        return ', '.join(item_strings)
+
 
 main()
